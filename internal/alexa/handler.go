b198a47dd4a5fbdbcb6d956a91adb250625152e3
Hikari Sakamoto <hikarisakamoto@pm.me>
2026-03-29 19:57:48 +0100

    adds basic structure  for handler

---
+++ internal/alexa/handler.go
@@ -0,0 +1,74 @@
+package alexa
+
+import (
+	"context"
+	"time"
+)
+
+const (
+	requestTimeout = 10 * time.Second
+	maxNameLength  = 100
+)
+
+const (
+	RequestTypeLaunch       = "LaunchRequest"
+	RequestTypeIntent       = "IntentRequest"
+	RequestTypeSessionEnded = "SessionEndedRequest"
+
+	IntentArrival      = "ArrivalIntent"
+	IntentDeparture    = "DepartureIntent"
+	IntentHelp         = "AMAZON.HelpIntent"
+	IntentStop         = "AMAZON.SpotIntent"
+	IntentCancel       = "AMAZON.CancelIntent"
+	IntentFallback     = "AMAZON.FallbackIntent"
+	IntentNavigateHome = "AMAZON.NavigateHomeIntent"
+)
+
+type Request struct {
+	Version string      `json:"version"`
+	Session Session     `json:"session"`
+	Body    RequestBody `json:"request"`
+}
+
+type Session struct {
+	Application Application `json:"application"`
+}
+
+type Application struct {
+	ApplicationID string `json:"applicationId"`
+}
+
+type RequestBody struct {
+	Type   string `json:"type"`
+	Intent Intent `json:"intent"`
+}
+
+type Intent struct {
+	Name  string          `json:"name"`
+	Slots map[string]Slot `json:"slots"`
+}
+
+type Slot struct {
+	Name string `json:"name"`
+}
+
+type Response struct {
+	Version  string       `json:"version"`
+	Response ResponseBody `json:"response"`
+}
+
+type ResponseBody struct {
+	OutputSpeech     *OutputSpeech `json:"outputSpeech,omitempty"`
+	ShouldEndSession bool          `json:"shouldEndSession"`
+}
+
+type OutputSpeech struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
+}
+
+type SheetsService interface {
+	EnsureHeaders(ctx context.Context) error
+	RecordArrival(ctx context.Context, name string) error
+	RecordDeparture(ctx context.Context, name string) error
+}
